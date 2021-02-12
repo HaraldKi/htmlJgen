d5ea841b17e32514e391e0eb6b498f9107854e45
Harald Kirsch <pifpafpuf@gmx.de>
2021-02-12 11:11:55 +0100

    Adds parameter isModule to methods that add script links to pages.
    Switches to java-8.

--- src/java/de/pifpafpuf/web/html/HtmlPage.java
+++ src/java/de/pifpafpuf/web/html/HtmlPage.java
@@ -11,7 +11,7 @@ public class HtmlPage implements Stringable {
   private final Html html = new Html("html");
   private final Html head = html.add("head");
   private final Html body = html.add("body");
-  private final List<String> bodyJsUrls = new ArrayList<>(5);
+  private final List<BodyJsUrl> bodyJsUrls = new ArrayList<>(5);
 
   /*+******************************************************************/
   public HtmlPage(String title) {
@@ -31,7 +31,7 @@ public class HtmlPage implements Stringable {
     .setAttr("rel", rel)
     .setAttr("href", href)
     ;
-    
+
     head.add(link);
     return link;
   }
@@ -40,15 +40,25 @@ public class HtmlPage implements Stringable {
   }
 
   /**
-   * add a javascript link to the page head.
+   * Adss a type="text/javascript" link to the page head.
    */
   public void addJs(String url) {
-    addJs(head, url);
+    addJs(head, url, false);
   }
-  
-  private void addJs(Html elem, String url) {
+
+  /**
+   * add a Javascript or Javascript module link to the page head.
+   */
+  public void addJs(String url, boolean isModule) {
+    addJs(head, url, isModule);
+  }
+
+  /**
+   * Adds a javascript script element to a given element.
+   */
+  public static void addJs(Html elem, String url, boolean isModule) {
     elem.add("script")
-    .setAttr("type", "text/javascript")
+    .setAttr("type", isModule ? "module" : "text/javascript")
     .setAttr("src", url);
   }
 
@@ -56,16 +66,19 @@ public class HtmlPage implements Stringable {
    * register a javascript url to be added to the body element just before
    * calling {@link #print(Appendable)
    */
-  public void registerBodyJs(String url)
-  {
-    bodyJsUrls.add(url);
+  public void registerBodyJs(String url) {
+    bodyJsUrls.add(new BodyJsUrl(url, false));
+  }
+
+  public void registerBodyJs(String url, boolean isModule) {
+    bodyJsUrls.add(new BodyJsUrl(url, isModule));
   }
 
   public void addMeta(String name, String content) {
     EmptyElem meta = new EmptyElem("meta")
     .setAttr("name", name)
     .setAttr("content", content);
-    
+
     head.add(meta);
   }
   public void addHeadElem(EmptyElem elem) {
@@ -82,8 +95,8 @@ public class HtmlPage implements Stringable {
   /*+******************************************************************/
   @Override
   public void print(Appendable out, int indent) throws IOException {
-    for(String url : bodyJsUrls) {
-      addJs(body, url);
+    for(BodyJsUrl jsUrl: bodyJsUrls) {
+      addJs(body, jsUrl.url, jsUrl.isModule);
     }
     out.append("<!DOCTYPE html>\n");
     html.print(out, indent);
@@ -92,5 +105,15 @@ public class HtmlPage implements Stringable {
   @Override
   public String toString() {
     return TextUtils.toString(this);
+  }
+
+  private static class BodyJsUrl {
+    public final String url;
+    public final boolean isModule;
+    public BodyJsUrl(String url, boolean isModule) {
+      this.url = url;
+      this.isModule = isModule;
+
+    }
   }
 }
